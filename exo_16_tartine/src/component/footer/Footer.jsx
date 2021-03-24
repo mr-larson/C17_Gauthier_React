77ce8086b9bb0865d2ba743fc08c5012da31eba6
GAUTHIER <gauthierdewit@gmail.com>
2021-03-24 17:39:58 +0100

    finition

--- exo_16_tartine/src/component/footer/Footer.jsx
+++ exo_16_tartine/src/component/footer/Footer.jsx
@@ -5,7 +5,7 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div className="bg-dark text-secondary d-flex justify-content-center align-items-center py-2">
+            <div className="myFooter bg-dark text-secondary d-flex justify-content-center align-items-center py-4">
                 <span>© copyright</span>
             </div>
         )
