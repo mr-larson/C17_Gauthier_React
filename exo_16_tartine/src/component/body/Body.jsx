ac06b33ca98ff23f7aecb86e1587049ad69ba356
GAUTHIER <gauthierdewit@gmail.com>
2021-03-24 17:09:04 +0100

    tartine fini

--- exo_16_tartine/src/component/body/Body.jsx
+++ exo_16_tartine/src/component/body/Body.jsx
@@ -1,29 +1,28 @@
 
 
 import React, { Component } from 'react'
+import Article from './Article'
 
 export default class Body extends Component {
+
+    nombreArticle = 2
+
     render() {
+        let currentPage
+
+        if (this.nombreArticle >=2) {
+            currentPage = 
+            <div>
+                <Article/> 
+                <Article/>
+            </div>
+        }
+        else{
+            currentPage = <h1> il n'y a pas d'article</h1>
+        }
         return (
-            <div class="container-fluid bg-dark p-4">
-                <div class="row">
-                    <div class="col-sm-4 d-flex justify-content-center">
-                        <img className="myimg m-2" src="./img/nicky.jpg" alt="nicky"/>
-                    </div>
-                    <div class="col-sm-offset-2 col-sm-5">
-                        <p className="text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos corporis repellendus error velit quo voluptates iste soluta dignissimos, quasi, non dolores nemo ad praesentium natus pariatur sequi culpa rem eius vero sit in perferendis expedita, debitis ab. Laboriosam maiores et nobis aperiam voluptatum? Vero eius dolorem rerum, pariatur neque eos.
-                        </p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-4 d-flex justify-content-center">
-                        <img className="myimg m-2" src="./img/nicky.jpg" alt="nicky"/>
-                    </div>
-                    <div class="col-sm-offset-2 col-sm-5">
-                        <p className="text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos corporis repellendus error velit quo voluptates iste soluta dignissimos, quasi, non dolores nemo ad praesentium natus pariatur sequi culpa rem eius vero sit in perferendis expedita, debitis ab. Laboriosam maiores et nobis aperiam voluptatum? Vero eius dolorem rerum, pariatur neque eos.
-                        </p>
-                    </div>
-                </div>
+            <div class="myBody container-fluid bg-dark p-4">
+                {currentPage}
             </div>
         )
     }
