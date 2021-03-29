574fe6fb70ebcd866a7d9bff6f2fedfc2ef792b0
GAUTHIER <gauthierdewit@gmail.com>
2021-03-29 13:45:43 +0200

    todolist

--- exo_25_state/src/component/footer/Footer.jsx
+++ exo_25_state/src/component/footer/Footer.jsx
@@ -1,42 +1,67 @@
 
 
 import React, { Component } from 'react'
+import reactDom from 'react-dom'
 
 export default class Footer extends Component {
 
+    clientInput = React.createRef()
+
+
+    // Tableau initial
     state = {
         clients:[
             {id: 1, nom:"Shun"},
             {id: 2, nom:"leon"},
             {id: 3, nom:"Saitama"}
         ],
-        compteur: 0
+        
     }
+    i = 4
+
+    // Ajoute un client au tableau initial
+    handleSubmit = (event) => {
+        event.preventDefault()
+        const ajout = this.state.clients.slice()
+        ajout.push({id: this.i, nom:(this.clientInput.current.value)})
+        this.i++
+
+        this.setState({clients: ajout})
+    }
+
+    // Supprime un client du tableau
+    handleDelete = id => {
+        const clients = this.state.clients.slice()
+        const index = clients.findIndex(client => client.id === id)
+        
+        clients.splice(index,1)
+
+        this.setState({ clients: clients})
+    }
+
     
-    handleClick = () => {
-        this.state.compteur++
-    }
 
     render() {
-
+        //ajout d'un titre
         const title = "Titre de la liste"
 
+        // event de suppression
         const elements = this.state.clients.map(client => (
-            <li>{client.nom} <button>X</button></li>
+            <li>{client.nom} <button onClick={() => this.handleDelete(client.id)}>X</button></li>
         ))
 
         return (
-            <footer className="bg-primary py-5">
+            <footer className="bg-primary p-5">
 
                 <h1 className="text-center">{title}</h1>
-                <button onClick={this.handleClick}>Click ici</button>
+                
 
                 <div className="d-flex justify-content-around align-items-center py-5">
                     <ul className="bg-danger text-center p-3">
                         {elements}
                     </ul>
-                    <form>
-                        <input type="text" placeholder="Ajouter un client"/>
+                    <form onSubmit={this.handleSubmit}>
+                        <input ref={this.clientInput} type="text" placeholder="Ajouter un client"/>
                         <button>Confirmer</button>
                     </form>
                 </div>
--- exo_25_state/src/component/header/Header.jsx
+++ exo_25_state/src/component/header/Header.jsx
@@ -1,38 +1,12 @@
 /*-------------- lien JSX --------------*/
 import Nav from './Nav'
-state = {
-    presentation: [
-        {
-            id: 1,
-            nom: "San",
-            prenom: "Goku"
-        },
-        {
-            id: 2,
-            nom: "Skywalker",
-            prenom: "Anakin"
-        },
-        {
-            id: 3,
-            nom: "McClane",
-            prenom: "John"
-        },
-        {
-            id: 4,
-            nom: "Ozhora",
-            prenom: "Tsubasa"
-        },
-        {
-            id: 5,
-            nom: "Onizuka",
-            prenom: "Eikichi"
-        }
-    ]
-}
 
 import React, { Component } from 'react'
 
 export default class Header extends Component {
+    state = {
+        
+    }
     render() {
         return (
             <header>
