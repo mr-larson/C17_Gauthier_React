c1826956a6ed6422a623342737c8f93dc9fc4c33
GAUTHIER <gauthierdewit@gmail.com>
2021-04-11 20:52:59 +0200

    section1

--- exo portfolio/src/component/body/Section1.jsx
+++ exo portfolio/src/component/body/Section1.jsx
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Section1 extends Component {
-    render() {
-        return (
-            <section>
-                
-            </section>
-        )
-    }
+export default function Section1() {
+    return (
+        <section>
+            <div className="container-fluid projects">
+                <h2>Last projects</h2>
+            </div>
+        </section>
+    )
 }
+
--- exo portfolio/src/component/header/Nav.jsx
+++ exo portfolio/src/component/header/Nav.jsx
@@ -6,7 +6,7 @@ export default function Nav() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark sticky-top">
             <div className="container">
-                <a className="navbar-brand text-success" href="###">PORTFOLIO</a>
+                <a className="navbar-brand" href="###">PORTFOLIO</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
