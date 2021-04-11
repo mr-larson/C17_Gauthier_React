45e6bdda16113f684fcf0a7b0023f81992667135
GAUTHIER <gauthierdewit@gmail.com>
2021-04-11 22:11:07 +0200

    footer

--- exo portfolio/src/component/body/Section1.jsx
+++ exo portfolio/src/component/body/Section1.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 export default function Section1() {
     return (
         <section>
-            <div className="container-fluid projects">
-                <h2>Last projects</h2>
-            </div>
+            
         </section>
     )
 }
--- exo portfolio/src/component/footer/Footer.jsx
+++ exo portfolio/src/component/footer/Footer.jsx
@@ -1,13 +1,30 @@
+import React from 'react'
 
+export default function Footer() {
+    return (
+        <footer>
+            <div className="container-fluid first-footer">
+                <h2><span>Thanks</span> for <span>watc</span>hing</h2>
+            </div>
+            <div className="container-fluid link-footer text-center text-white">
+                <section className="mb-4">
+                    <a className="btn btn-outline-light  rounded-circle m-2" href="###" role="button"><i class="fab fa-facebook-f"></i></a>
+                    <a className="btn btn-outline-light rounded-circle m-2" href="###" role="button"><i class="fab fa-twitter"></i></a>
 
-import React, { Component } from 'react'
+                    <a className="btn btn-outline-light rounded-circle m-2" href="###" role="button"><i class="fab fa-google"></i></a>
 
-export default class Footer extends Component {
-    render() {
-        return (
-            <footer>
-                
-            </footer>
-        )
-    }
+                    <a className="btn btn-outline-light rounded-circle m-2" href="###" role="button"><i class="fab fa-instagram"></i></a>
+
+                    <a className="btn btn-outline-light rounded-circle m-2" href="###" role="button"><i class="fab fa-linkedin-in"></i></a>
+
+                    <a className="btn btn-outline-light rounded-circle m-2" href="###" role="button"><i class="fab fa-github"></i></a>
+                </section>
+            </div>
+            <div className="text-center p-3 bg-dark">
+                <span className="text-light">© 2020 Copyright</span>
+            </div>
+        </footer>
+            
+    )
 }
+
