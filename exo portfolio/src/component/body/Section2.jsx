365a85be30a4a64e797084670295d2f2ed70994b
GAUTHIER <gauthierdewit@gmail.com>
2021-04-12 13:01:18 +0200

    card

--- exo portfolio/src/component/body/Section2.jsx
+++ exo portfolio/src/component/body/Section2.jsx
@@ -1,11 +1,39 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class Section2 extends Component {
-    render() {
-        return (
-            <section>
-                
-            </section>
-        )
-    }
+export default function Section2() {
+    return (
+        <section className="card container-fluid">
+            <h3 className="text-light py-5">Lastest projects</h3>
+                <div class="row">
+
+                    <div class="box col-sm-12 col-md-offset-1 col-md-3">
+                        <span></span>
+                        <div className="content">
+                            <h2>Card one</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quisquam nam sapiente nemo. Suscipit, iusto!</p>
+                            <a href="###"> Read more</a>
+                        </div>
+                    </div>
+
+                    <div class="box col-sm-12 col-md-4">
+                        <span></span>
+                        <div className="content">
+                            <h2>Card two</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quisquam nam sapiente nemo. Suscipit, iusto!</p>
+                            <a href="###"> Read more</a>
+                        </div>
+                    </div>
+
+                    <div class="box col-sm-12 col-md-3">
+                        <span></span>
+                        <div className="content">
+                            <h2>Card three</h2>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quisquam nam sapiente nemo. Suscipit, iusto!</p>
+                            <a href="###"> Read more</a>
+                        </div>
+                    </div>
+
+                </div>
+        </section>
+    )
 }
