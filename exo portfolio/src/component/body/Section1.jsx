6993931dbbfcab94c020c8f57ab7716578bbf401
GAUTHIER <gauthierdewit@gmail.com>
2021-04-13 11:23:44 +0200

    section3 ok

--- exo portfolio/src/component/body/Section1.jsx
+++ exo portfolio/src/component/body/Section1.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Section1() {
     return (
-        <section>
+        <section id="section1" className="">
             
         </section>
     )
--- exo portfolio/src/component/body/Section3.jsx
+++ exo portfolio/src/component/body/Section3.jsx
@@ -3,14 +3,12 @@ import React from 'react'
 
 export default function Section3() {
     return (
-        <div>
-            <section id="section3"  className="container-fluid p-0">
-                <div>
-                    <h4 className="text-white">I accept your challenge</h4>
-                    <a href="#" className="btn btn-dark">Contact me</a>
-                </div>
-            </section>
-        </div>
+        <section id="section3"  className="container-fluid p-0">
+            <div>
+                <h4 className="text-dark">I accept your challenge</h4>
+                <a href="#" className="btn btn-dark">Contact me</a>
+            </div>
+        </section>
     )
 }
 
